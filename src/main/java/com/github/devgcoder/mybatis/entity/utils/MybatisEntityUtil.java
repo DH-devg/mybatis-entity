e7e96a0596137dc757466a2bd00af2c308a3cf91
duheng <dh@zhihuigongjiang.com>
2021-01-28 11:14:44 +0800

    add MybatisEntityUtil

---
+++ src/main/java/com/github/devgcoder/mybatis/entity/utils/MybatisEntityUtil.java
@@ -0,0 +1,41 @@
+package com.github.devgcoder.mybatis.entity.utils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author duheng
+ * @Date 2021/1/28 10:53
+ */
+public class MybatisEntityUtil {
+
+	public static <E> List<E> parseObjctList(List<Map<String, Object>> list, Class<E> sourceType) {
+		List<E> resultList = new ArrayList<>();
+		if (null == list || list.size() <= 0) {
+			return resultList;
+		}
+		for (Map<String, Object> map : list) {
+			resultList.add(parseObject(map, sourceType));
+		}
+		return resultList;
+	}
+
+	public static <E> E parseObject(Map<String, Object> map, Class<E> sourceType) {
+		try {
+			E result = sourceType.newInstance();
+			Field[] fields = sourceType.getDeclaredFields();
+			for (Field field : fields) {
+				Object value = map.get(field.getName());
+				Field entityField = result.getClass().getDeclaredField(field.getName());
+				entityField.setAccessible(true);
+				entityField.set(result, value);
+			}
+			return result;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+}
